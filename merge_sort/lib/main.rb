f004a253559af0da805469e80e21f147befbc606
gabrielpendas42 <gabriel.pendas@gmail.com>
2022-02-23 12:58:03 -0700

    refactored merge and merge sort methods

--- merge_sort/lib/main.rb
+++ merge_sort/lib/main.rb
@@ -1,24 +1,30 @@
 def merge(left_array, right_array)
   result = []
-  while !left_array.empty? && !right_array.empty?
-    if left_array[0] < right_array[0]
-      result.push(left_array.shift)
-    else
-      result.push(right_array.shift)
+   until left_array.empty? && right_array.empty?
+      if  left_array[0] <= right_array[0]
+        result << left_array[0] 
+        left_array.delete_at(0)
+      else  
+        result << right_array[0] 
+        right_array.delete_at(0)
+      end
+
+      if right_array.empty?
+        result.concat(left_array)
+        left_array.clear
+      elsif left_array.empty?
+        result.concat(right_array)
+        right_array.clear
+      end
     end
-  end
   result
 end
 
-p merge([3,7,10,23], [1,9,15,75])
-  
-
 def merge_sort(array)
- return array if array.size < 2 
- left = merge_sort(array.slice(0...array.size/2))
- right = merge_sort(array.slice(array.size/2...array.size))
-merge(left,right)
- 
+  return array if array.size < 2
+  left = array.take(array.size/2)
+  right = array.drop(array.size/2)
+  array = merge(merge_sort(left), merge_sort(right))
 end
-
-p merge_sort([5,7,6,8,9,2,4,5,3])
+  
+p merge_sort([5,57,7,6,8,9,2,4,5,3,-23,33,-1,56,9,-10,100])
